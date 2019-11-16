30be999c6da11667f712b23510a1e9b82684a254
kingkoor <kingkoor@yahoo.com>
2019-11-16 11:38:27 -0500

    Assignment 2

---
+++ src/pages/comment/comment.ts
@@ -0,0 +1,55 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
+import {Validators, FormBuilder, FormGroup } from '@angular/forms';
+
+import { Comment } from '../../shared/comment';
+
+/**
+ * Generated class for the CommentPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-comment',
+  templateUrl: 'comment.html',
+})
+export class CommentPage {
+
+  commentForm: FormGroup;
+  comment: Comment;  
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+    public viewCtrl: ViewController,
+    private formBuilder: FormBuilder ) {
+      this.commentForm = this.formBuilder.group({
+        author: ['', Validators.required],
+        comment: ['', Validators.required],
+        rating: [5, Validators.required]
+      });
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad CommentPage');
+  }
+
+  dismiss() {
+    this.viewCtrl.dismiss(this.comment);
+  }
+
+  onSubmit() {
+    this.comment = this.commentForm.value;
+    this.comment.date = new Date().toISOString();
+    console.log(this.comment);
+    this.viewCtrl.dismiss(this.comment);
+    this.comment = null;
+    this.commentForm.reset({
+      author: '',
+      comment: '',
+      rating: 5
+    });
+  }
+
+}
--- src/pages/dishdetail/dishdetail.ts
+++ src/pages/dishdetail/dishdetail.ts
@@ -1,16 +1,17 @@
 import { Component, Inject } from '@angular/core';
-import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, 
+  ToastController, ActionSheetController, ModalController  } from 'ionic-angular';
 import { Dish } from '../../shared/dish';
 import { Comment } from '../../shared/comment';
 import { FavoriteProvider } from '../../providers/favorite/favorite';
+import {CommentPage } from '../comment/comment';
 
 /**
  * Generated class for the DishdetailPage page.
  *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
+ * See http://ionicframework.com/docs/components/#navigation for more info
+ * on Ionic pages and navigation.
  */
-
 @IonicPage()
 @Component({
   selector: 'page-dishdetail',
@@ -26,20 +27,21 @@ export class DishdetailPage {
   constructor(public navCtrl: NavController, public navParams: NavParams,
     @Inject('BaseURL') private BaseURL,
     private favoriteservice: FavoriteProvider,
-    private toastCtrl: ToastController) {
+    private toastCtrl: ToastController,
+    private actionSheetCtrl: ActionSheetController,
+    private modalCtrl: ModalController) {
     this.dish = navParams.get('dish');
     this.favorite = favoriteservice.isFavorite(this.dish.id);
     this.numcomments = this.dish.comments.length;
     let total = 0;
     this.dish.comments.forEach(comment => total += comment.rating );
     this.avgstars = (total/this.numcomments).toFixed(2);
-
-    
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad DishdetailPage');
   }
+
   addToFavorites() {
     console.log('Adding to Favorites', this.dish.id);
     this.favorite = this.favoriteservice.addFavorite(this.dish.id);
@@ -49,4 +51,43 @@ export class DishdetailPage {
       duration: 3000}).present();
   }
 
+  presentActionSheet() {
+    let actionSheet = this.actionSheetCtrl.create({
+      title: 'Select Actions',
+      buttons: [
+        {
+          text: 'Add To Favorites',
+          handler: () => {
+            this.addToFavorites();
+          }
+        },
+        {
+          text: 'Add Comment',
+          handler: () => {
+            console.log('Add Comment clicked');
+            this.openCommentModal();
+          }
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    }); 
+    actionSheet.present();
+  }
+
+  openCommentModal() {
+    let modal = this.modalCtrl.create(CommentPage);
+    modal.onDidDismiss(comment => {
+      console.log(comment);
+      if (comment) {
+        this.dish.comments.push(comment);
+      }
+    });
+    modal.present();
+  }
 }
